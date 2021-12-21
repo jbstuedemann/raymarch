090d0d66fe5a157d79ee6aa85f258e8d80b52f33
jbstuede <jbstuedemann@gmail.com>
2021-12-21 14:27:58 -0600

    Initial commit

---
+++ build/CMakeFiles/raymarch.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raymarch.dir/src/main.cpp.o"
+  "CMakeFiles/raymarch.dir/src/main.cpp.o.d"
+  "raymarch"
+  "raymarch.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raymarch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/raymarch.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for raymarch.
+# This may be replaced when dependencies are built.
