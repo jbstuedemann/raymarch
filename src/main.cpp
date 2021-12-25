68de2aedbe9941e22d5300347b421b7baa7cec1a
James Stuedemann <jbstuedemann@gmail.com>
2021-12-24 22:58:41 -0600

    Main file

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,10 @@ int PIXEL_HEIGHT = 1080;
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
-
+int runTestCase(std::string testCase) {
     Parser* parser;
     try { 
-        parser = new Parser("../tests/Test1.scene", false);
+        parser = new Parser((testCase+".scene"), false);
     }
     catch (std::exception &ex) {
         std::cout << ex.what() << std::endl;
@@ -25,12 +24,25 @@ int main(int argc, char* argv[]) {
     renderer->renderScene();
 
     std::ofstream imageFile;
-    imageFile.open("../Test1.ppm");
+    imageFile.open(testCase+".ppm");
     imageFile << "P3\n" << PIXEL_WIDTH << '\t' << PIXEL_HEIGHT << '\t' << 255 << '\n';
     for (uint8_t pixel : renderer->getRender()) {
         imageFile << (int)pixel << '\t';
     }
     imageFile.close();
+
+    system(("pnmtopng "+testCase+".ppm > "+testCase+".png && rm "+testCase+".ppm").data());
+
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    vector<std::string> testCases {"../tests/Test1", "../tests/Test2", "../tests/Test3"};
+
+    for (std::string test : testCases) {
+        std::cout << "Running case: " << test << std::endl;
+        if (runTestCase(test) != 0) return -1;
+    }
 
     return 0;
 }
